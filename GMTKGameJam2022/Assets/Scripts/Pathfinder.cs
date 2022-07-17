38679b4f51240c6771fc88fa60918620215493b1
Thomas <thetardis1968@gmail.com>
2022-07-17 10:46:12 -0500

    Can cancel out of  most ability uses

--- GMTKGameJam2022/Assets/Scripts/Pathfinder.cs
+++ GMTKGameJam2022/Assets/Scripts/Pathfinder.cs
@@ -183,7 +183,6 @@ public class Pathfinder
         }
         else
         {
-            Debug.Log("found no path");
             var debugNodes = new List<T>();
             foreach (var item in closed)
             {
